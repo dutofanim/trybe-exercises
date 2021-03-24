b443315199e64b29857beec1f952907e3628e340
dutofanim <etofanim@gmail.com>
2021-03-24 19:30:19 -0300

    Resolução dos exercícios 2, 3, 4 e 5.

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise_5-verificaAngulo.js
@@ -0,0 +1,17 @@
+let angle1 = 60;
+let angle2 = 60;
+let angle3 = 60;
+
+if ((angle1 + angle2 + angle3) === 180) {
+  console.log('true');
+} else {
+  console.log('false')
+}
+
+if (angle1 < 0) {
+  console.log('Erro no valor do ângulo 1')
+} else if (angle2 < 0) {
+  console.log('Erro no valor do ângulo 2')
+} else if (angle3 < 0) {
+  console.log('Erro no valor do ângulo 3')
+}
